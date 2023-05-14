9feb4c67634601b92dcc32da38ac0f4323eeb773
Isaac Batista <isaacbatst@gmail.com>
2023-05-14 15:53:38 -0300

    improve preview styles

--- src/application/components/SheetPreview/SheetPreviewAbilities.tsx
+++ src/application/components/SheetPreview/SheetPreviewAbilities.tsx
@@ -9,11 +9,11 @@ const SheetPreviewAbilities = () => {
   const roleAbilities = useSelector(selectPreviewRoleAbilities)
 
   return (
-    <div className='py-2'>
-      <div className='flex flex-col md:flex-row gap-12'>
+    <div className='py-2 px-5'>
+      <div className='flex flex-col md:flex-row gap-4 md:gap-12'>
         <div className='flex-1'>
           <h3 className='font-semibold mb-6'>Habilidades de Raça</h3>
-          <ul>
+          <ul className='flex flex-col gap-6'>
             {raceAbilities?.map(ability => (
               <SheetPreviewAbility 
                 ability={ability}
@@ -23,9 +23,10 @@ const SheetPreviewAbilities = () => {
             ))}
           </ul>
         </div>
+        <hr className="md:hidden opacity-40" />
         <div className='flex-1'>
           <h3 className='font-semibold mb-6'>Habilidades de Classe</h3>
-          <ul>
+          <ul className='flex flex-col gap-6'>
             {roleAbilities?.map(ability => (
               <SheetPreviewAbility 
                 ability={ability}
--- src/application/components/SheetPreview/SheetPreviewAbility.tsx
+++ src/application/components/SheetPreview/SheetPreviewAbility.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const SheetPreviewAbility = ({ability, translatedName}: Props) => {
   return (
-    <li key={ability.name} className='mb-6'>
+    <li key={ability.name}>
       <p className='font-semibold mb-1'>{translatedName}</p>
       {ability.effects.map((effect, index) => (
         <p key={index} className='text-sm mb-1'>{effect.description}</p>
